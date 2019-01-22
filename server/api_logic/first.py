8d299d47752413e4e18de78d4010c128c016b61b
ortizk <ortizk@live.com>
2019-01-21 18:09:48 -0800

    first commit

---
+++ server/api_logic/first.py
@@ -0,0 +1,12 @@
+import os
+import csv
+
+def read_tsv(): 
+	cwd = os.getcwd()
+	print(cwd)
+	with open(f'{cwd}/data/log.txt', 'r', encoding=None) as tsvin:
+		tsvin = csv.reader(tsvin, delimiter='\t')
+		for row in tsvin:
+			print('hello')
+
+read_tsv()
