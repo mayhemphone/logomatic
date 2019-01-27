9d9802417230cf46147c291cd01b0cf9dd5e3064
mayhemphone <mayhemphone@gmail.com>
2019-01-26 21:38:34 -0800

    Shit is working

--- server/api_logic/write_tsv.py
+++ server/api_logic/write_tsv.py
@@ -34,25 +34,58 @@ for item in log_list:
 f_log_list = []
 
 for record in log_list:
-	f_auto_type = record['auto_type'].ljust(2, ' ') 
+	if len(record['auto_type']) == 1:
+		f_auto_type = '0' + record['auto_type']
+		f_auto_type = f_auto_type.ljust(2, ' ')
+	else:
+		f_auto_type = record['auto_type'].ljust(2, ' ')
 	f_cart_number = record['cart_number'].ljust(8, ' ')
+	# if record['title'] == 'VOICETRACK':
+	# 	f_title = ' ' + record['title'].ljust(24, ' ')
+	# else:
 	f_title = record['title'].ljust(24, ' ')
 	f_length = record['length'].ljust(4, ' ')
 	f_temp = record['temp'].ljust(1, ' ')
-	f_month = record['month'].ljust(3, ' ')
-	f_day = record['day'].ljust(3, ' ')
-	f_year = record['year'].ljust(2, ' ')
-	f_hour = record['hour'].ljust(1, ' ')
+	if len(record['month']) == 1:
+		f_month = '0' + record['month']
+		f_month = f_month.ljust(2, ' ')
+		# f_month = '0' + record['month'].ljust(2, ' ')
+	else:
+		f_month = record['month'].ljust(2, ' ')
+	# f_month = record['month'].ljust(3, ' ')
+	if len(record['day']) == 1:
+		f_day = '0' + record['day']
+		f_day = f_day.ljust(2, ' ')
+		# f_day = '0' + record['day'].ljust(3, ' ')
+	else:
+		f_day = record['day'].ljust(2, ' ')
+	# f_day = record['day'].ljust(3, ' ')
+	if len(record['year']) == 1:
+		f_year = '0' + record['year']
+		f_year = f_year.ljust(2, ' ')
+		# f_year = '0' + record['year'].ljust(2, ' ')
+	else:
+		f_year = record['year'].ljust(2, ' ')
+	# f_year = record['year'].ljust(2, ' ')
+	if len(record['hour']) == 1:
+		f_hour = '0' + record['hour']
+		f_hour = f_hour.ljust(2, ' ')
+		# f_hour = '0' + record['hour'].ljust(2, ' ')
+	else:
+		f_hour = record['hour'].ljust(2, ' ')
+	# f_hour = record['hour'].ljust(2, ' ')
 	f_log_list.append([f_auto_type, f_cart_number, f_title, f_length, f_temp, f_month, f_day, f_year, f_hour])
 
 print(len(f_log_list))
 
 cwd = os.getcwd()
 with open(f'{cwd}/data/output.txt', 'wt') as out_file:
-	tsv_writer = csv.writer(out_file, delimiter=' ', quoting=csv.QUOTE_NONE, escapechar='None', quotechar='')
+	# tsv_writer = csv.writer(out_file, delimiter=' ', quoting=csv.QUOTE_NONE, escapechar=' ')
+	# tsv_writer = csv.writer(out_file, delimiter=',', quoting=csv.QUOTE_NONE, doublequote = True, escapechar=',')
 	for element in f_log_list:
-		tsv_writer.writerow(element)
-	print(type(tsv_writer))
+		# tsv_writer.writerow(element)
+		print(*element, file=out_file)
+	# print(type(tsv_writer))
 
 
 
